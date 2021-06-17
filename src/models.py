116638ae4cf820c1f369e18924eeeb89e519ef75
tapuks <david_berdiell@hotmail.com>
2021-06-17 16:07:18 +0000

    ok

--- src/models.py
+++ src/models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    nombre = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
@@ -14,6 +14,49 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "nombre": self.nombre,
+            # do not serialize the password, its a security breach
+        }
+
+class Abuelos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    apellido = db.Column(db.String(80), unique=False, nullable=False)
+    edad = db.Column(db.Integer(3), unique=False, nullable=False)
+    referencia_id = db.Column(db.Integer(3), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "edad": self.edad,
+            "is_active": self.is_active
+            # do not serialize the password, its a security breach
+        }
+    
+class Padres(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    apellido = db.Column(db.String(80), unique=False, nullable=False)
+    edad = db.Column(db.Integer(3), unique=False, nullable=False)
+    abuelo_id = db.Column(db.Integer(3), ForeignKey('abuelos.id'))
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "edad": self.edad,
+            "abuelo_id": self.abuelo_id,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
